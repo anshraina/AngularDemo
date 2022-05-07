c97f41c76c1a77ab44ec2fde99790fe2a4c75f44
anshraina <anshraina13@gmail.com>
2022-05-07 15:47:46 +0530

    Initial commit

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})    
-export class AppComponent {
-  /*title = 'practice2';
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+
+
+  title = 'practice2';
   name: string = "Ansh";
   classABC: string = "Raina"; // String name = "Ans Raina"; 
   pageTitle: string = "Angular Basics";
@@ -41,5 +43,12 @@ export class AppComponent {
 
   onOff() {
     this.isVisible = !this.isVisible;
-  } */
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+ 
+
 }
